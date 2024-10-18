ce47af65f4739d373eab9bed33116fdb2fcaca26
StefanoDaniello <stefanodaniello68@gmail.com>
2024-10-18 14:19:15 +0200

    glueStack v2 whit reactNative

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, ButtonText } from "./components/ui/button";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider mode="light">
+      <View style={styles.container}>
+        <Button size="md" variant="solid" action="primary">
+          <ButtonText>Hello World!</ButtonText>
+        </Button>
+        <Text className="text-red-500">oooo</Text>
+        <Text className="text-red-500">oooo</Text>
+        <Text className="text-yellow-500">oooo</Text>
+      </View>
+    </GluestackUIProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
